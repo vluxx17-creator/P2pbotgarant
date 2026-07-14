c777642b6269058769440316f9d7c09523d3080f
DiMaggio <vluxx17@gmail.com>
2026-07-14 18:13:49 +0300

    Delete web.py

--- web.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-async def handle(request):
-    return web.Response(text="Bot is running")
-
-def start_web():
-    app = web.Application()
-    app.router.add_get('/', handle)
-    web.run_app(app, host='0.0.0.0', port=8080)
